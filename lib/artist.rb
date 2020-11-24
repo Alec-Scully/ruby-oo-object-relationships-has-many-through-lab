ec70c19aa22113c88810f361131201954e687835
Alec Scully <ScullyAlec@gmail.com>
2020-11-24 15:50:04 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,35 @@
+require 'pry'
 
+class Artist
+    attr_reader :name
+    attr_accessor :genre
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do | song |
+            # binding.pry
+            song.artist == self
+        end
+    end
+
+    def new_song(name, genre)
+        new_song = Song.new(name, self, genre)
+    end
+
+    def genres
+        artist_genre = self.songs.map do | song |
+            song.genre
+        end
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
+class Genre
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        genre_songs = Song.all.select do | songs |
+            songs.genre == self
+        end
+    end
+
+    def artists
+        genre_artists = self.songs.map do | songs |
+            songs.artist
+        end
+    end
+end
