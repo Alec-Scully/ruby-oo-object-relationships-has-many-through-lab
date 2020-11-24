0144096931c2c2dcb65bf3b9bca01f52bc00a31d
Alec Scully <ScullyAlec@gmail.com>
2020-11-24 15:49:55 -0500

    commit tree 157b43840f83290a4c29cf34fd263830afeaf122

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,31 @@
+class Doctor
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        doc_appointments = Appointment.all.select do | appointments |
+            appointments.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        new_appointment = Appointment.new(date, patient, self)
+    end
+
+    def patients
+        self.appointments.map do | appointments |
+            appointments.patient
+        end
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,31 @@
+class Patient
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        new_appointment = Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        pat_appo = Appointment.all.select do | appointments |
+            appointments.patient == self
+        end
+    end
+
+    def doctors
+        pat_doctors = self.appointments.map do | appointments |
+            appointments.doctor
+        end
+    end
+end 
